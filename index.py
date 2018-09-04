a98d1003e1330541d1ec531aa9edc27ee9638e1d
anonape <18473081+anonape@users.noreply.github.com>
2018-09-04 18:06:38 -0400

    integrated firebase database into web app

--- index.py
+++ index.py
@@ -1,8 +1,37 @@
 from flask import Flask, request, render_template, jsonify
 import os
 import pymongo
+import pyrebase
+
+try:
+    from keys import keys
+except:
+    print("Keys File not Found. Online Access")
+
+API_KEY = os.environ.get('API_KEY') or keys['api_key']
+AUTH_DOMAIN = os.environ.get('AUTH_DOMAIN') or keys['auth_domain']
+DB_URL = os.environ.get('DB_URL') or keys['db_url']
+PROJECT_ID = os.environ.get('PROJECT_ID') or keys['project_id']
+STORAGE_BUCKET = os.environ.get('STORAGE_BUCKET') or keys['storage_bucket']
+MESSAGING_SENDER_ID = os.environ.get('MESSAGING_SENDER_ID') or keys['messaging_sender_id']
+SERVICE_ACCOUNT = os.environ.get('SERVICE_ACCOUNT') or keys['service_account']
+
+config = {
+  "apiKey": API_KEY,
+  "authDomain": AUTH_DOMAIN,
+  "databaseURL": DB_URL,
+  "projectId": PROJECT_ID,
+  "storageBucket": STORAGE_BUCKET,
+  "messagingSenderId": MESSAGING_SENDER_ID,
+  "serviceAccount": SERVICE_ACCOUNT
+}
 
 app = Flask(__name__)
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password("tester@llov.com", "tester")
+db = firebase.database()
 
 @app.route('/')
 def index():
@@ -17,9 +46,9 @@ def login():
 def formsPage():
     pass
 
-@app.route('/data', methods=['GET'])
+@app.route('/data')
 def dataPage():
-    pass
+    return jsonify(db.get().val())
 
 @app.route('/donations')
 def donationsPage():
