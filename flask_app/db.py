9d92e12cc25592fb82b4f3a66d4338d11fa85283
Anna Miller <annamiller@Annas-MacBook-Pro.local>
2018-11-09 22:29:07 -0500

    Updated templates to reflect new params being passed from routes.py, added some variables in dashboard with jinja templating, changed data.html to a dedicated dashboard.html page

--- flask_app/db.py
+++ flask_app/db.py
@@ -1,76 +1,97 @@
-""" This module does blah blah blah """
+""" PLEASE DON'T CHANGE LINES 1-17 """
 
 import pyrebase
 from flask_app import errors
-from flask_app import DB
+
+try:
+    import keys
+except IOError:
+    print("Keys File not Found. Online Access")
+
+FIREBASE = pyrebase.initialize_app(keys.FIREBASE_KEYS)
+FIREBASE_AUTH = FIREBASE.auth()
+FIREBASE_USER = FIREBASE_AUTH.sign_in_with_email_and_password(
+                                                             "tester@llov.com",
+                                                             "tester")
+DB = FIREBASE.database()
 
 
-def data_from(collection):
+def data_from(request1, request2):
+    """
+    Temporary method that will be removed once data calls are better
+        conceptualized (See notes on method below)
+    """
+    return {'test1': sanitize_user_input(request1),
+            'test2': sanitize_user_input(request2),
+            'test3': 'test3'
+            }
+
+
+def get_dashboard_data():
+    data = {}
+    data['events'] = get_events()
+    data['donations'] = get_donations()
+
+    return data
+
+
+def get_donations():
     """
     description
     """
-
-    donation_data = [{'key': item.key(), 'val': item.val()} for item in
-                     DB.child(collection).get().each()]
-    donation_keys = [key.lower() for key in donation_data[0]['val'].keys()]
-    return donation_data, donation_keys
+    return DB.child("donations").get()
 
 
 def add_foster_dog(data):
     """
     description
     """
-    for key, value in data:
-        if value is None:
-            raise errors.InvalidUsage('This view is gone', status_code=410)
-        else:
-            key = sanitize_user_input(key)
-            value = sanitize_user_input(value)
 
-    DB.child("fosterdogs").push(data)
+    clean_foster_data = sanitize_data(data)
+    DB.child("fosterdogs").push(clean_foster_data)
 
 
 def add_donation(data):
     """
+    ALL donations should be added to the corresponding account
+        BEFORE being added to firebase. So this method shoud ONLY be called
+        after the add donation method from plaid_ctrl.py is called! Ex:
+        New donation is being made. We pass the amount through the plaid_ctrl
+        add_donation() method and the Plaid API takes care of the actual
+        transaction, all we have to do is make the POST request (by calling
+        the add_donation() method in plaid_ctrl). AFTER the transaction is
+        successful, we can figure out how to write this method so that it
+        asynchronously updates firebase.
+    """
+
+    # See above notes
+    # DB.child("donations").push(data)
+
+    return None
+
+
+def get_events():
+    """
     description
     """
 
-    for key, value in data:
-        if value is None:
-            raise errors.InvalidUsage('This view is gone', status_code=410)
-        else:
-            key = sanitize_user_input(key)
-            value = sanitize_user_input(value)
-
-    DB.child("donations").push(data)
+    return DB.child("events").get()
 
 
 def add_foster(data):
     """
     description
     """
-    for key, value in data:
-        if value is None:
-            raise errors.InvalidUsage('This view is gone', status_code=410)
-        else:
-            key = sanitize_user_input(key)
-            value = sanitize_user_input(value)
-
-    DB.child("fosters").push(data)
+    clean_data = sanitize_data(data)
+    DB.child("fosters").push(clean_data)
 
 
 def add_volunteer(data):
     """
     description
     """
-    for key, value in data:
-        if value is None:
-            raise errors.InvalidUsage('This view is gone', status_code=410)
-        else:
-            key = sanitize_user_input(key)
-            value = sanitize_user_input(value)
-
-    DB.child("volunteers").push(data)
+    clean_data = sanitize_data(data)
+    DB.child("volunteers").push(clean_data)
 
 
 def sanitize_user_input(user_input):
@@ -78,3 +99,18 @@ def sanitize_user_input(user_input):
     description
     """
     return user_input
+
+
+def sanitize_data(data):
+    """
+    description
+    """
+
+    for key, value in data:
+        if value is None:
+            raise errors.InvalidUsage('All fields must be filled', status_code=410)
+        else:
+            key = sanitize_user_input(key)
+            value = sanitize_user_input(value)
+
+    return data
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,55 +1,24 @@
 """ This module does blah blah blah """
 
-import datetime
 from flask import render_template, request
-from flask_app import DB
 from flask_app import APP
-from flask_app import plaid_ctrl
+from flask_app import plaid_ctrl, db
 
 
 @APP.route('/')
-@APP.route('/index')
-def index():
+@APP.route('/dashboard')
+def dashboard():
     """
     description
     """
-
-    table = "donations"
-    donation_data, donation_keys = DB.data_from(table)
-    donation_amount = 0
-    donation_dates = []
-    donation_amounts = []
-    donation_types_graph = {'Cash': 0, 'Check': 0, 'Venmo': 0, 'Cashapp': 0}
-
-    for donation in donation_data:
-        donation_amount += float(donation['val']['amount'])
-        date = donation['val']['timestamp'].split()
-        date = date[0].split('-')
-        date = date[1] + '-' + date[2] + '-' + date[0]
-        donation_dates.insert(0, date)
-        donation_amounts.insert(0, float(donation['val']['amount']))
-        donation_types_graph[donation['val']['source']] \
-            += float(donation['val']['amount'])
-    donation_dates, donation_amounts = zip(*sorted(zip(donation_dates,
-                                                       donation_amounts)))
-    table = "events"
-    event_data, event_keys = db.DB.data_from(table)
-    return render_template('main.html',
-                           donation_pie_labels=donation_types_graph.keys(),
-                           donation_pie_values=donation_types_graph.values(),
-                           donation_dates=donation_dates,
-                           donation_amounts=donation_amounts,
-                           donation_graph_data=donation_data,
-                           donation_amount=donation_amount,
-                           event_data=event_data,
-                           event_keys=event_keys,
-                           page="Main")
+    data = db.get_dashboard_data()
+    return render_template('dashboard.html', page="Dash", data=data)
 
 
 @APP.route('/login')
 def login():
     """
-    description
+    Going to move the notes below into separate authorization controller
     """
     """
     Account management actions like changing pwd should be in separate route
@@ -77,16 +46,20 @@ def login():
     return render_template('login.html')
 
 
-@APP.route('/dog', methods=['POST'])
+@APP.route('/dog', methods=['GET', 'POST'])
 def post_dog():
     """
     description
     """
-    new_foster_dog_data = {}
-    # for field in form:
-    #     new_foster_dog_data[field.name] = field.data
 
-    db.DB.add_foster_dog(new_foster_dog_data)
+    if request.method == 'POST':
+        new_foster_dog_data = {}
+        # WTForms needed for this:
+        # for field in form:
+        #     new_foster_dog_data[field.name] = field.data
+
+        db.add_foster_dog(new_foster_dog_data)
+
     return render_template('dog.html', page="Foster Dog")
 
 
@@ -96,43 +69,50 @@ def post_volunteer():
     description
     """
 
-    data = {
-        'first_name': request.form.get('firstName'),
-        'last_name': request.form.get('lastName'),
-        'street': request.form.get('street'),
-        'city': request.form.get('city'),
-        'state': request.form.get('state'),
-        'zipcode': request.form.get('zipcode'),
-        'phone_number': request.form.get('number'),
-        'volunteering': {
-            'can_volunteer': request.form.get('volunteering'),
-            'longterm': request.form.get('long-term-foster'),
-            'shortterm': request.form.get('short-term-foster'),
-            'emergency': request.form.get('emergency-foster'),
-            'coord': request.form.get('coordinating'),
-            'flyers': request.form.get('putting-up-flyers'),
-            'dogwalking': request.form.get('dog-walking'),
-            'fundraising': request.form.get('fundraisers'),
-            'adoptions': request.form.get('helping-at-adoptions'),
-            'transporting': request.form.get('transporting'),
-            'vet': request.form.get('vet-appointments'),
-            'other': request.form.get('volunteering-other')
-        },
-        'adoption_fostering': {
-            'can_adopt': request.form.get('adopting'),
-            'can_foster': request.form.get('fostering'),
-            'dogTypes': {
-                'female': request.form.get('female'),
-                'male': request.form.get('male'),
-                'small': request.form.get('small'),
-                'large': request.form.get('large'),
-                'breeds': request.form.get('breeds'),
-                'other': request.form.get('fostering-other')
-            }
-        }
-    }
+    new_volunteer_data = {}
+    # I want to convert listing the data manually like below
+    # to list data automatically like in this for loop:
+    # Need to implement WTForms for this
+    # for field in form:
+    #     new_volunteer_data[field.name] = field.data
 
-    db.DB.add_volunteer(data)
+    # data = {
+    #     'first_name': request.form.get('firstName'),
+    #     'last_name': request.form.get('lastName'),
+    #     'street': request.form.get('street'),
+    #     'city': request.form.get('city'),
+    #     'state': request.form.get('state'),
+    #     'zipcode': request.form.get('zipcode'),
+    #     'phone_number': request.form.get('number'),
+    #     'volunteering': {
+    #         'can_volunteer': request.form.get('volunteering'),
+    #         'longterm': request.form.get('long-term-foster'),
+    #         'shortterm': request.form.get('short-term-foster'),
+    #         'emergency': request.form.get('emergency-foster'),
+    #         'coord': request.form.get('coordinating'),
+    #         'flyers': request.form.get('putting-up-flyers'),
+    #         'dogwalking': request.form.get('dog-walking'),
+    #         'fundraising': request.form.get('fundraisers'),
+    #         'adoptions': request.form.get('helping-at-adoptions'),
+    #         'transporting': request.form.get('transporting'),
+    #         'vet': request.form.get('vet-appointments'),
+    #         'other': request.form.get('volunteering-other')
+    #     },
+    #     'adoption_fostering': {
+    #         'can_adopt': request.form.get('adopting'),
+    #         'can_foster': request.form.get('fostering'),
+    #         'dogTypes': {
+    #             'female': request.form.get('female'),
+    #             'male': request.form.get('male'),
+    #             'small': request.form.get('small'),
+    #             'large': request.form.get('large'),
+    #             'breeds': request.form.get('breeds'),
+    #             'other': request.form.get('fostering-other')
+    #         }
+    #     }
+    # }
+
+    db.add_volunteer(new_volunteer_data)
     return render_template('volunteer.html', page="Volunteers")
 
 
@@ -142,49 +122,70 @@ def post_to_fosters():
     description
     """
 
-    data = {
-        'first_name': request.form.get('first_name'),
-        'last_name': request.form.get('last_name'),
-        'street': request.form.get('street'),
-        'city': request.form.get('city'),
-        'state': request.form.get('state'),
-        'zipcode': request.form.get('zipcode'),
-        'can_adopt_more': request.form.get('can_adopt_more'),
-        'comments': request.form.get('comments'),
-        'timestamp': str(datetime.datetime.now())
-    }
-    db.DB.add_foster(data)
+    new_foster_data = {}
+    # I want to convert listing the data manually like below
+    # to list data automatically like in this for loop:
+    # Need to implement WTForms for this
+    # for field in form:
+    #     new_foster_data[field.name] = field.data
+
+    # data = {
+    #     'first_name': request.form.get('first_name'),
+    #     'last_name': request.form.get('last_name'),
+    #     'street': request.form.get('street'),
+    #     'city': request.form.get('city'),
+    #     'state': request.form.get('state'),
+    #     'zipcode': request.form.get('zipcode'),
+    #     'can_adopt_more': request.form.get('can_adopt_more'),
+    #     'comments': request.form.get('comments'),
+    #     'timestamp': str(datetime.datetime.now())
+    # }
+
+    db.add_foster(new_foster_data)
     return render_template('foster.html', page="Fosters")
 
 
-@APP.route('/donation', methods=['POST'])
+@APP.route('/donation', methods=['GET', 'POST'])
 def post_to_donation():
     """
     description
     """
 
-    data = {
-        'first_name': request.form.get('firstName'),
-        'last_name': request.form.get('lastName'),
-        'amount': request.form.get('amount'),
-        'comments': request.form.get('comments'),
-        'source': request.form.get('source'),
-        'timestamp': str(datetime.datetime.now())
-    }
-    db.DB.add_donation(data)
+    if request.method == 'POST':
+        new_donation_data = {}
+        # I want to convert listing the data manually like below
+        # to list data automatically like in this for loop:
+        # Need to implement WTForms for this
+        # for field in form:
+        #     new_donation_data[field.name] = field.data
+
+        # data = {
+        #     'first_name': request.form.get('firstName'),
+        #     'last_name': request.form.get('lastName'),
+        #     'amount': request.form.get('amount'),
+        #     'comments': request.form.get('comments'),
+        #     'source': request.form.get('source'),
+        #     'timestamp': str(datetime.datetime.now())
+        # }
+        db.add_donation(new_donation_data)
+
     return render_template('donation.html', page="Donations")
 
 
 @APP.route('/data', methods=['POST', 'GET'])
 def return_data():
     """
-    description
+    I like the modular aspect of this method in that it allows
+        you to make a general request and depending on the
+        contents of that request it will use a different specialized method
+
+    However, this needs to be rewritten more cleanly and in the appropriate
+        controller file. It also needs to be more descriptive.
+        Are you requesting data from Plaid API or Firebase?
     """
 
-    get_data = None
     balance = None
-    plaid_data = None
-    get_keys = None
+    plaid_data = ""
     table = request.args.get('table')
 
     if table is None:
@@ -193,7 +194,7 @@ def return_data():
     if table == "donations":
         plaid_data = plaid_ctrl.get_plaid_data(request.args.get('request'))
 
-    get_data, get_keys = db.DB.data_from(table)
+    get_data, get_keys = db.data_from(table, "test")
     return render_template('data.html', data=get_data, get_keys=get_keys,
                            page=table, balance=balance, plaid_data=plaid_data)
 
@@ -201,7 +202,7 @@ def return_data():
 @APP.route('/update', methods=['POST', 'GET'])
 def update():
     """
-    description
+    Same as above method, needs to be more descriptive. What is being updated?
     """
     print(request.args)
     return return_data()
@@ -210,6 +211,6 @@ def update():
 @APP.route('/on_app_load')
 def on_app_load():
     """
-    description
+    See above ^
     """
     pass
