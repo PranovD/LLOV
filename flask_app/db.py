595c9e95efdf80ee9f96c2867c30c91011c09643
Michal <meaganelizamayford@gmail.com>
2018-11-19 18:38:18 -0500

    Tables display data from FireBase again

--- flask_app/db.py
+++ flask_app/db.py
@@ -14,113 +14,39 @@ FIREBASE_USER = FIREBASE_AUTH.sign_in_with_email_and_password(
                                                              "tester@llov.com",
                                                              "tester")
 DB = FIREBASE.database()
+white_listed_collections = ["Dogs", "Person", "donations", "events"]
 
 
-def data_from(request1, request2):
+def get_firebase_collection(collection):
     """
-    Temporary method that will be removed once data calls are better
-        conceptualized (See notes on method below) - MA
-
-    I think we should instead create standard getters & setters for donations,
-        events, and dogs, but the modular aspect of how this method works in
-        terms of the /data route would be an excellent set up for requesting
-        data from volunteers/fosters/& donors. Currently, there is a general
-        "Profile" collection in Firebase where basic info such as
-        name, email, etc is stored for all people. There are
-        separate collections for volunteers, fosters, & donors that store the
-        attributes that are specific to each group. Because of the way it's
-        setup in there currently, I think this would work very well for
-        requesting people related collections. - MA
-    """
-    return {'test1': sanitize_user_input(request1),
-            'test2': sanitize_user_input(request2),
-            'test3': 'test3'
-            }
-
-
-def get_dashboard_data():
-    """
-    Calls the relevant methods to populate a data object for the dashboard.
-        This object is returned and later on is divided into more specific
-        variables in templates using Jinja.
-
-    Method created when I was trying to separate application layers,
-        particularly to help replace data_from() method - MA
-    """
-    data = {}
-    data['events'] = get_events()
-    data['donations'] = get_donations()
-    data['members'] = mc_ctrl.get_members()
-
-    return data
-
-
-def get_donations():
-    """
-    Retrieves "donations" collection in Firebase and returns as a JSON object
-
-    Not sure if this returns a JSON object or pyre-object (whatever)? - MA
-    """
-    return DB.child("donations").get()
-
-
-def add_foster_dog(data):
-    """
-    Adds a new foster dog to the "dogs" collection in Firebase based on the
-        passed in data
+    Makes a get call to FireBase based on what collection the user wants to see
+    Checks requested collection against a whitelist of available collections
     """
 
-    clean_foster_data = sanitize_data(data)
-    DB.child("fosterdogs").push(clean_foster_data)
+    if collection not in white_listed_collections:
+        print(collection)
+        raise errors.InvalidUsage("We are currently not storing \
+                                  this data.", status_code=410)
+    return DB.child(collection).get().val()
 
 
-def add_donation(data):
+def add_firebase_document(collection, data):
     """
-    Adds a donation to the "donations" collection in Firebase based on the
-        passed in data after the transaction is successful
-
-    ALL donations should be added to the corresponding account
-        BEFORE being added to firebase. So this method shoud ONLY be called
-        after the add donation method from plaid_ctrl.py is called! Ex:
-        New donation is being made. We pass the amount through the plaid_ctrl
-        add_donation() method and the Plaid API takes care of the actual
-        transaction, all we have to do is make the POST request (by calling
-        the add_donation() method in plaid_ctrl). AFTER the transaction is
-        successful, we can figure out how to write this method so that it
-        asynchronously updates firebase. - MA
+    Description
     """
 
-    # See above notes - MA
-    # DB.child("donations").push(data)
-
-    return None
-
-
-def get_events():
-    """
-    Retrieves "events" collection in Firebase and returns as a JSON object
-
-    not sure if this returns a JSON object or pyre-object (whatever)? - MA
-    """
-
-    return DB.child("events").get()
-
-
-def add_foster(data):
-    """
-    Adds a foster volunteer with passed in data to "volunteers"
-        collection in Firebase
-    """
     clean_data = sanitize_data(data)
-    DB.child("fosters").push(clean_data)
+    clean_collection = sanitize_user_input(collection)
 
+    if clean_collection not in white_listed_collections:
+        raise errors.InvalidUsage("We are currently not storing \
+                                  this data.", status_code=410)
+    try:
+        DB.child(clean_collection).push(clean_data)
 
-def add_volunteer(data):
-    """
-    Adds a volunteer with passed in data to "volunteers" collection in Firebase
-    """
-    clean_data = sanitize_data(data)
-    DB.child("volunteers").push(clean_data)
+    except:
+        raise errors.InvalidUsage("We could not add your information \
+                                  at this time.", status_code=410)
 
 
 def sanitize_user_input(user_input):
